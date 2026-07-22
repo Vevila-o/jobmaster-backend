fe1a5f1d04310b1b2dd05240808ab0bdb342eb27
vevila <vevila@5xruby.com>
2026-07-22 18:08:09 +0800

    user edit&show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  # 使用者是0 管理者是1
+
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+
+    if @user.update(user_pramas)
+      redirect_to user_path(@user), notice: "變身！"
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def user_pramas
+    params.require(:user).permit(:name, :email, :role)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  enum :role, { "一般使用者": 0, "管理者": 1 }
 end
