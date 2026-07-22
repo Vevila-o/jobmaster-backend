78b3e9655e8fe8a1d4f726a1bcef798f4c2c8ff2
vevila@5xruby.com <vevila@5xruby.com>
2026-07-22 10:43:17 +0800

    add erb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,37 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  # new 不是 create!! 這個是暫時存在記憶體裡面
+  def new
+    @task = Task.new
+  end
+
+  # 因為ruby 4.0 有資安設計 不能直接寫new(params[:task])
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
+
+  # 新增
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: "すごい！成功した!"
+    
+    else
+      render :new
+    end
+  end
+
+
+  # 修改
+
+
+
 end
