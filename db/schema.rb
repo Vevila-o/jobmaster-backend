bee2770a323894853043c4a90e03f19f2358e9ed
vevila <vevila@5xruby.com>
2026-08-14 18:40:41 +0800

    add: users, tasks migrate change null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_31_125933) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_14_102633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -43,8 +43,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_31_125933) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "email"
-    t.string "name"
+    t.string "email", null: false
+    t.string "name", null: false
     t.string "password_digest"
     t.string "role", default: "normal"
     t.datetime "updated_at", null: false
