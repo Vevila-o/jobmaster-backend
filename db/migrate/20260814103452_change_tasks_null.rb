3dcffcd0610b7204bf2e322fc8bb40e5722b7c8d
vevila <vevila@5xruby.com>
2026-08-14 18:42:17 +0800

    add: users, tasks migrate change null

---
+++ db/migrate/20260814103452_change_tasks_null.rb
@@ -0,0 +1,5 @@
+class ChangeTasksNull < ActiveRecord::Migration[8.1]
+  def change
+    change_column_null :tasks, :title, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_14_102633) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_14_103452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_14_102633) do
     t.integer "priority"
     t.datetime "start_time"
     t.integer "task"
-    t.string "title"
+    t.string "title", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
   end
