d5db5ebf8dad29af1f33ee98765398aaeee2c1ca
vevila <vevila@5xruby.com>
2026-08-18 18:01:02 +0800

    修正: 在password驗證中新增on: :create條件

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :email, format: { with: /\A[^@]+@[^@]+\z/, allow_blank: true, message: I18n.t("errors.messages.invalid") }, uniqueness: { message: I18n.t("errors.messages.taken"), allow_blank: true }, presence: { message: I18n.t("errors.messages.blank") }
 
-  validates :password, presence: { message: I18n.t("errors.messages.blank") }
+  validates :password, presence: { message: I18n.t("errors.messages.blank") }, on: :create
 
   attribute :role, :string, default: "normal"
 end
