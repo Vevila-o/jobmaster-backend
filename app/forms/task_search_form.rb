3ad7c840439766e2e8800d5e79a67574a40aa553
vevila <vevila@5xruby.com>
2026-08-26 22:21:34 +0800

    feat: 更新搜尋功能，支援標題和狀態篩選

--- app/forms/task_search_form.rb
+++ app/forms/task_search_form.rb
@@ -1,7 +1,11 @@
 class TaskSearchForm
   include ActiveModel::Model
     attr_accessor :title, :status
+    ALLOWED_STATUS = Task.statuses.keys.freeze
+
+    validates :status, inclusion: { in: ALLOWED_STATUS, message: I18n.t("errors.messages.not_include") }, allow_blank: true
     def search
+      return Task.none unless valid?
       scope = Task.all
       scope = scope.where("title LIKE ?", "%#{title}%") if title.present?
       scope = scope.where(status: status) if status.present?
