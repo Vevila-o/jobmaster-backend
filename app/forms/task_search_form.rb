222463ebbe2e2ec3c95a6d06a9313ed14923a620
vevila <vevila@5xruby.com>
2026-09-04 20:53:07 +0800

    新增：將任務search form與controller整合使用者資訊，確保任務僅顯示當前使用者的任務

--- app/forms/task_search_form.rb
+++ app/forms/task_search_form.rb
@@ -1,12 +1,13 @@
 class TaskSearchForm
   include ActiveModel::Model
-    attr_accessor :title, :status
+    attr_accessor :title, :status, :user
+
     ALLOWED_STATUS = Task.statuses.keys.freeze
 
     validates :status, inclusion: { in: ALLOWED_STATUS, message: I18n.t("errors.messages.not_include") }, allow_blank: true
     def search
       return Task.none unless valid?
-      scope = Task.all
+      scope = user.tasks
       scope = scope.where("title LIKE ?", "%#{title}%") if title.present?
       scope = scope.where(status: status) if status.present?
       scope
