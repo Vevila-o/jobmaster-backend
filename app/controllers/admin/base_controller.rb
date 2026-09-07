7220aeb4c4255d1a325f98b1bce2066d6c499920
vevila <vevila@5xruby.com>
2026-09-07 16:27:53 +0800

    step24：添加檢查管理員權限的方法

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,10 @@
 
 class Admin::BaseController < ApplicationController
+  before_action :check_admin
+
+  def check_admin
+    unless current_user.adminstrator?
+      redirect_to tasks_path, alert: t("navigation.auth.no_permission")
+    end
+  end
 end
